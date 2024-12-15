591cde9c4c0dd179ce3ee6e4b1d2432a8d974a5a
acasmar0506b <adcasmar03@gmail.com>
2024-12-15 12:53:14 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$dbname = "bd_acm";
+$username = "acasmar0506b";
+$password = "adrip200";
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error en la conexión a la base de datos: " . $e->getMessage());
+}
+?>
