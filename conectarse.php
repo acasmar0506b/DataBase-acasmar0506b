57167759947413f6d40d426853670e93ff2fb674
acasmar0506b <adcasmar03@gmail.com>
2024-12-15 12:55:26 +0100

    Create conectarse.php

---
+++ conectarse.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Connect</title>
+  <link rel="stylesheet" href="proyecto.css" title="Color">
+</head>
+<body>
+<header>
+  <h1>Connect DataBase</h1>
+  <nav>
+  <ul class="nav-buttons">
+    <li><a href="iniciar.php">Login</a></li>
+    <li><a href="registro.php">Register</a></li>
+  </ul>
+  </nav>
+  </header>
+</body>
+</html>
